13832439c96299ff14c4e038eedef0ea00b933b0
n3xistence <maxinies00@gmail.com>
2024-03-31 03:25:23 +0200

    feat(jwt): add JWT refresh token table

---
+++ migrations/0003_jwt_refresh_token.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "refresh_token" (
+    user_id     INT PRIMARY KEY  NOT NULL,
+    token       TEXT             NOT NULL
+);
+
+CREATE INDEX user_id ON "refresh_token" (user_id);
